3abc74c9b7b0338ce76ecb1e1e5a89510d901cfe
Phil Ganem <philipganem@gmail.com>
2023-05-10 18:52:46 -0400

    Dingus

--- src/App.js
+++ src/App.js
@@ -10,19 +10,15 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="container h-screen flex flex-col">
-      <div className=" font-code h-full w-full bg-[#1e2326] text-white flex flex-col">
+      <div className=" container h-screen w-screen flex flex-col font-code bg-[#1e2326] text-white">
         <Heads></Heads>
         <NavBar></NavBar>
-        <div className="flex-grow">
-          <About></About>
-          <Experience></Experience>
-          <Blog></Blog>
-          <Contact></Contact>
-        </div>
+        <About></About>
+        <Experience></Experience>
+        <Blog></Blog>
+        <Contact></Contact>
         <Cmd></Cmd>
       </div>
-    </div>
   );
 }
 
