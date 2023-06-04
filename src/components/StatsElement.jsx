d1c6b39d84a63b19e3f33b5589168dae9e45a160
Phil Ganem <philipganem@gmail.com>
2023-06-04 13:58:34 -0400

    ele

---
+++ src/components/StatsElement.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function StatsElement(props) {
+  return (
+    <div className="flex">
+      <li className="w-full bg-gray-100 p-1 rounded-md">
+      </li>
+    </div>
+  );
+}
