2b2e4949896f9b5414ef4c68fce8e9a759dc4926
Phil Ganem <philipganem@gmail.com>
2023-06-22 23:14:43 -0400

    bg

--- src/App.js
+++ src/App.js
@@ -10,38 +10,42 @@ import Stats from "./components/Stats";
 
 function App() {
   return (
-    <div className="h-screen w-screen flex flex-row font-code bg-[#1e2326] text-white overflow-y-auto justify-center">
-      
-      {/* <div className="hidden max-w-[15rem] sm:visible sm:flex-col sm:flex">
+    <div className="computerScreen h-screen w-screen font-code text-white overflow-y-auto">
+      <div className="scanLines w-full h-full justify-center flex flex-row overflow-y-auto">
+        {/* <div className="hidden max-w-[15rem] sm:visible sm:flex-col sm:flex">
         <img src={fire} alt="fire" className="w-10 h-10 "></img>
       </div> */}
 
-      <div className="flex flex-col max-w-[70rem] w-full">
-        <div>
-          <Heads></Heads>
-          <NavBar></NavBar>
-        </div>
-        <div className="space-y-6 h-full w-full">
-          <div className="mx-4 pb-4">
-            <div id="home" className="flex text-2xl md:text-3xl justify-center md:justify-start pt-4">
-              <h2 className="font-bold">Character Sheet</h2>
-            </div>
-            <div className="space-y-4 py-4">
-              <About></About>
-              <Stats></Stats>
-              <Experience></Experience>
-              <Contact></Contact>
+        <body className="flex flex-col max-w-[70rem] w-full">
+          <div>
+            <Heads></Heads>
+            <NavBar></NavBar>
+          </div>
+          <div className="space-y-6 h-full w-full">
+            <div className="mx-4 pb-4">
+              <div
+                id="home"
+                className="flex text-2xl md:text-3xl justify-center md:justify-start pt-4"
+              >
+                <h2 className="font-bold">Character Sheet</h2>
+              </div>
+              <div className="space-y-4 py-4">
+                <About></About>
+                <Stats></Stats>
+                <Experience></Experience>
+                <Contact></Contact>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </body>
 
-      {/* <div className="hidden max-w-[15rem] flex-wrap sm:visible sm:flex-col sm:flex">
+        {/* <div className="hidden max-w-[15rem] flex-wrap sm:visible sm:flex-col sm:flex">
         <img src={fire} alt="fire" className="w-10 h-10"></img>
       </div> */}
 
-      <div className="fixed bottom-0 w-full">
-        <Cmd></Cmd>
+        <div className="fixed bottom-0 w-full">
+          <Cmd></Cmd>
+        </div>
       </div>
     </div>
   );
